b74566b14cc9009eab5b226589606a869938f0f3
Amish Mittal <amishmittal@outlook.com>
2023-12-09 22:46:20 +0530

    refeactor gameLobby

---
+++ src/app/components/gameLobby.tsx
@@ -0,0 +1,205 @@
+import {
+  Body1,
+  Button,
+  Divider,
+  InfoLabel,
+  LargeTitle,
+  Persona,
+  Switch,
+  useToastController,
+} from "@fluentui/react-components";
+import styles from "../page.module.css";
+import CustomToaster from "./toaster";
+import { mapToArray } from "../services/conversions";
+import { Player } from "../interfaces/gameRoomState";
+import {
+  MessageTypes,
+  PlayerReadyMessageData,
+  StartGameMessageData,
+} from "../interfaces/serverMessages";
+import {
+  Copy24Regular,
+  AirplaneTakeOff24Filled,
+  AirplaneTakeOff24Regular,
+} from "@fluentui/react-icons";
+import { useContext, useMemo } from "react";
+import { GameContext } from "../game/gameContext";
+import { GameContextData } from "../interfaces/contexts";
+
+export default function GameLobby({
+  gameCode,
+  gameContext,
+}: {
+  gameCode: string;
+  gameContext: GameContextData;
+}) {
+  const { dispatchToast } = useToastController("toaster");
+
+  const allPlayerReady = useMemo(() => {
+    if (gameContext && gameContext.room) {
+      for (const player of gameContext.room.state.players.values()) {
+        if (!player.isReady) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }, [JSON.stringify(gameContext?.room?.state.players)]);
+
+  if (gameContext.room) {
+    return (
+      <>
+        <div className={styles.flexcontainer} style={{ flexFlow: "column" }}>
+          <div className={styles.flexitemmargin} style={{ height: "15vh" }}>
+            <div className={styles.flexcontainer} style={{ flexFlow: "row" }}>
+              <div className={styles.flexitemmargin}>
+                <div
+                  className={styles.flexcontainer}
+                  style={{ flexFlow: "column" }}
+                >
+                  <div className={styles.flexitemmargin}>
+                    <Body1>Game Code</Body1>
+                    <InfoLabel
+                      info={
+                        "Share this code with others to allow them to join."
+                      }
+                    />
+                  </div>
+                  <div className={styles.flexitemmargin}>
+                    <div
+                      className={styles.flexcontainer}
+                      style={{ flexFlow: "row" }}
+                    >
+                      <div className={styles.flexitemmargin}>
+                        <LargeTitle style={{ letterSpacing: "12px" }}>
+                          {gameCode}
+                        </LargeTitle>
+                      </div>
+                      <div className={styles.flexitemmargin}>
+                        <Button
+                          icon={<Copy24Regular />}
+                          title="Copy to Clipboard"
+                          appearance="subtle"
+                          onClick={() => {
+                            navigator.clipboard
+                              .writeText(
+                                `I'm inviting you to join my Restart. game! Use the code ${gameCode} or link ${window.location.href} to join.`,
+                              )
+                              .then(() => {
+                                dispatchToast(
+                                  <CustomToaster text="Copied to clipboard" />,
+                                  { intent: "success" },
+                                );
+                              })
+                              .catch((err) => {
+                                console.error(
+                                  "Unable to copy to clipboard: ",
+                                  err,
+                                );
+                              });
+                          }}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.flexitemmargin}>
+                <Divider vertical appearance="brand" />
+              </div>
+              <div className={styles.flexitemmargin}>
+                <div
+                  className={styles.flexcontainer}
+                  style={{ flexFlow: "column", alignItems: "start" }}
+                >
+                  <div className={styles.flexitemmargin}>
+                    <Body1>Connected Players</Body1>
+                  </div>
+                  <div className={styles.flexitemmargin}>
+                    <div className={styles.flexcontainer}>
+                      {mapToArray(
+                        gameContext.room.state.players,
+                        (_: string, player: Player) => {
+                          return (
+                            <div
+                              className={styles.flexitemmargin}
+                              style={{ marginRight: "16px" }}
+                              key={player.playerID}
+                            >
+                              <Persona
+                                avatar={{
+                                  name: player.playerName,
+                                  image: { src: player.avatar },
+                                  title: player.playerName,
+                                  size: 56,
+                                }}
+                                textPosition="below"
+                                size="medium"
+                                primaryText={
+                                  player.isReady ? "Ready" : "Waiting"
+                                }
+                                presence={{
+                                  status: player.isReady ? "available" : "away",
+                                }}
+                              />
+                            </div>
+                          );
+                        },
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.flexitemmargin}>
+            <Switch
+              label="Ready"
+              checked={
+                gameContext.room.state.players.get(gameContext.room.sessionId)
+                  ?.isReady
+              }
+              onChange={(_, checked) => {
+                const playerReadyMessageData: PlayerReadyMessageData = {
+                  playerReady: checked.checked,
+                };
+                gameContext.room?.send(
+                  MessageTypes.playerReady,
+                  playerReadyMessageData,
+                );
+              }}
+            />
+          </div>
+          <div className={styles.flexitemmargin}>
+            {allPlayerReady ? (
+              gameContext.room.state.gameAdmin ===
+              gameContext.room.sessionId ? (
+                <Button
+                  appearance="primary"
+                  size="large"
+                  icon={<AirplaneTakeOff24Filled />}
+                  onClick={() => {
+                    const startGameMessage: StartGameMessageData = {
+                      startGame: true,
+                    };
+                    gameContext.room?.send(
+                      MessageTypes.startGame,
+                      startGameMessage,
+                    );
+                  }}
+                >
+                  Start Game
+                </Button>
+              ) : (
+                <Body1>Waiting for game admin to start</Body1>
+              )
+            ) : (
+              <Body1>Waiting for everyone to get ready</Body1>
+            )}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
--- src/app/game/[gameCode]/page.tsx
+++ src/app/game/[gameCode]/page.tsx
@@ -32,6 +32,7 @@ import {
   StartGameMessageData,
 } from "@/app/interfaces/serverMessages";
 import CustomUseUser from "@/app/components/customUseUser";
+import GameLobby from "@/app/components/gameLobby";
 
 // This component relies on an already created game client
 export default function Game({ params }: { params: { gameCode: string } }) {
@@ -40,18 +41,6 @@ export default function Game({ params }: { params: { gameCode: string } }) {
   const { dispatchToast } = useToastController("toaster");
 
   const { gameContext, setGameContext } = useContext(GameContext);
-
-  const allPlayerReady = useMemo(() => {
-    if (gameContext && gameContext.room) {
-      for (const player of gameContext.room.state.players.values()) {
-        if (!player.isReady) {
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
-  }, [JSON.stringify(gameContext?.room?.state.players)]);
 
   // This useEffect connects to an already existing room ID for users directly landing on route /game/[gameCode]
   useEffect(() => {
@@ -87,160 +76,13 @@ export default function Game({ params }: { params: { gameCode: string } }) {
   }, [gameContext?.client]);
 
   if (gameContext?.room) {
-    return (
-      <>
-        <div className={styles.flexcontainer} style={{ flexFlow: "column" }}>
-          <div className={styles.flexitemmargin} style={{ height: "15vh" }}>
-            <div className={styles.flexcontainer} style={{ flexFlow: "row" }}>
-              <div className={styles.flexitemmargin}>
-                <div
-                  className={styles.flexcontainer}
-                  style={{ flexFlow: "column" }}
-                >
-                  <div className={styles.flexitemmargin}>
-                    <Body1>Game Code</Body1>
-                    <InfoLabel
-                      info={
-                        "Share this code with others to allow them to join."
-                      }
-                    />
-                  </div>
-                  <div className={styles.flexitemmargin}>
-                    <div
-                      className={styles.flexcontainer}
-                      style={{ flexFlow: "row" }}
-                    >
-                      <div className={styles.flexitemmargin}>
-                        <LargeTitle style={{ letterSpacing: "12px" }}>
-                          {params.gameCode}
-                        </LargeTitle>
-                      </div>
-                      <div className={styles.flexitemmargin}>
-                        <Button
-                          icon={<Copy24Regular />}
-                          title="Copy to Clipboard"
-                          appearance="subtle"
-                          onClick={() => {
-                            navigator.clipboard
-                              .writeText(
-                                `I'm inviting you to join my Restart. game! Use the code ${params.gameCode} or link ${window.location.href} to join.`,
-                              )
-                              .then(() => {
-                                dispatchToast(
-                                  <CustomToaster text="Copied to clipboard" />,
-                                  { intent: "success" },
-                                );
-                              })
-                              .catch((err) => {
-                                console.error(
-                                  "Unable to copy to clipboard: ",
-                                  err,
-                                );
-                              });
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.flexitemmargin}>
-                <Divider vertical appearance="brand" />
-              </div>
-              <div className={styles.flexitemmargin}>
-                <div
-                  className={styles.flexcontainer}
-                  style={{ flexFlow: "column", alignItems: "start" }}
-                >
-                  <div className={styles.flexitemmargin}>
-                    <Body1>Connected Players</Body1>
-                  </div>
-                  <div className={styles.flexitemmargin}>
-                    <div className={styles.flexcontainer}>
-                      {mapToArray(
-                        gameContext.room.state.players,
-                        (_: string, player: Player) => {
-                          return (
-                            <div
-                              className={styles.flexitemmargin}
-                              style={{ marginRight: "16px" }}
-                              key={player.playerID}
-                            >
-                              <Persona
-                                avatar={{
-                                  name: player.playerName,
-                                  image: { src: player.avatar },
-                                  title: player.playerName,
-                                  size: 56,
-                                }}
-                                textPosition="below"
-                                size="medium"
-                                primaryText={
-                                  player.isReady ? "Ready" : "Waiting"
-                                }
-                                presence={{
-                                  status: player.isReady ? "available" : "away",
-                                }}
-                              />
-                            </div>
-                          );
-                        },
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.flexitemmargin}>
-            <Switch
-              label="Ready"
-              checked={
-                gameContext.room.state.players.get(gameContext.room.sessionId)
-                  ?.isReady
-              }
-              onChange={(_, checked) => {
-                const playerReadyMessageData: PlayerReadyMessageData = {
-                  playerReady: checked.checked,
-                };
-                gameContext.room?.send(
-                  MessageTypes.playerReady,
-                  playerReadyMessageData,
-                );
-              }}
-            />
-          </div>
-          <div className={styles.flexitemmargin}>
-            {allPlayerReady ? (
-              gameContext.room.state.gameAdmin ===
-              gameContext.room.sessionId ? (
-                <Button
-                  appearance="primary"
-                  size="large"
-                  icon={<AirplaneTakeOff24Filled />}
-                  onClick={() => {
-                    const startGameMessage: StartGameMessageData = {
-                      startGame: true,
-                    };
-                    gameContext.room?.send(
-                      MessageTypes.startGame,
-                      startGameMessage,
-                    );
-                  }}
-                >
-                  Start Game
-                </Button>
-              ) : (
-                <Body1>Waiting for game admin to start</Body1>
-              )
-            ) : (
-              <Body1>Waiting for everyone to get ready</Body1>
-            )}
-          </div>
-        </div>
-      </>
-    );
+    if (!gameContext.room.state.isGameStarted) {
+      return <GameLobby gameCode={params.gameCode} gameContext={gameContext} />;
+    } else {
+      return <Spinner />;
+    }
   }
+
   return (
     <>
       <Spinner />
